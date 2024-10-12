3edc4912c2a2ef3e2e21c39c1c0e7dfdf64ffed5
alejo <alejoviviani12@gmail.com>
2024-10-12 20:20:57 -0300

    Test Product Repared

--- src/test/test.php
+++ src/test/test.php
@@ -1,19 +1,16 @@
 <?php
+
 namespace App\Entity;
-// require_once 'bootstrap.php';
-require_once __DIR__ . '/../../vendor/autoload.php';
-// use App\Entity\ProductService;
-use Doctrine\ORM\EntityManagerInterface;
-require_once '../Service/productService.php';
-// Obtener el EntityManager desde el bootstrap
 require_once __DIR__ . '/../bootstrap.php';
-// ?string $name, ?int $price, ?int $stock, ?string $discount, ?string $imgUrl, ?Purchase $purchase_product, EntityManagerInterface $entityManager
-// Crear un nuevo servicio de producto
-$productService = new ProductService("Laptop", 1000, 10, 0, "null", $entityManager);
-$productService = new ProductService("Laptop", 1000, 10, 0, "null", $entityManager);
+require_once '../Service/productService.php';
+use Doctrine\ORM\EntityManagerInterface;
+
+$productService = new ProductService($entityManager);
+// $product = $productService->createProduct("Laptop", 1000, 10, 0, "null", $entityManager);
 
 // Leer un producto (con ID 1, por ejemplo)
 $product = $productService->readProduct(1);
+
 if ($product) {
     echo "Producto: " . $product->getName() . PHP_EOL;
     echo "Stock actual: " . $product->getStock() . PHP_EOL;
@@ -24,7 +21,9 @@ if ($product) {
 // Incrementar stock en 5
 $res = $productService->adjustStock(1, 5, 'increment');
 echo $res . PHP_EOL;
+echo $product . PHP_EOL;
 
-// Decrementar stock en 3
+// // Decrementar stock en 3
 $res = $productService->adjustStock(1, 3, 'decrement');
 echo $res . PHP_EOL;
+echo $product . PHP_EOL; 
