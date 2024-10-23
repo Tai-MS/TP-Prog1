2040478fe3216b51d0aa13902274783fa96bbd34
alejo <alejoviviani12@gmail.com>
2024-10-23 16:34:44 -0300

    add PurchaseButton

--- public/js/purchase.js
+++ public/js/purchase.js
@@ -1,21 +1,22 @@
-const $productos = document.querySelector('#products-button');
-const $cookies = document.$cookies;
-const $productsID = document.querySelectorAll('.bought-products');
-const $quantity = document.querySelectorAll('.quantity-products');
+const $productos = document.querySelector('#form-button');
 const $listaProductos = document.querySelector('#product-list');
 
 window.addEventListener('DOMContentLoaded', e=> {
     const cartItems = JSON.parse(localStorage.getItem('cart'));
     if(cartItems) {
-        const productId = cartItems.map(object => {
-            return object.id;
+        const product = cartItems.map(object => {
+            return {'id': object.id, 'quantity': object.quantity};
         });
 
-        const $ul = document.createElement('ul');
-        const $li = document.createElement('li');
-        const $deleteButton = document.createElement('button');
-
-
+        product.forEach(element => {
+            fetch('../../public/purchase-handler.php', {method: 'POST', body: JSON.stringify({'productsID': [element.id]}), headers: {'Content-Type': 'application/json'}})
+                .then(data => data.json())
+                    .then(data => {
+                        data.quantity = element.quantity;
+                        frontData(data);
+                    })
+                        .catch(err => console.log(err));
+        });
     } else {
         const $label = document.createElement('label');
         $label.innerHTML = 'Por el momento, no hay artículos en el carrito';
@@ -23,22 +24,56 @@ window.addEventListener('DOMContentLoaded', e=> {
     }
 })
 
-$productos.addEventListener('click', e=> {
-    e.preventDefault;
-    const body = {
-        'userEMail': $cookies.replace('userEmail=', '').replace('%', '@'),
+$productos.addEventListener('submit', e=> {
+    e.preventDefault();
+    const $productsID = Array.from(document.querySelectorAll('.bought-products'))
+    .map(product => product.id);
+
+    const $quantity = Array.from(document.querySelectorAll('.quantity-products'))
+    .map(input => input.id);
+
+    const dataForm = {
+        'userEMail': document.cookie.replace('userEmail=', '').replace('%', '@'),
         'productsID': $productsID,
         'quantity': $quantity
     };
-    const dataForm = new FormData(body);
 
-    fetch('../purchase-handler.php', {method: 'POST', body: JSON.stringify(dataForm), headers: {'Content-Type': 'application/json'}})
+    console.log(dataForm);
+
+    fetch('../../public/purchase-handler.php', {method: 'POST', body: JSON.stringify(dataForm), headers: {'Content-Type': 'application/json'}})
         .then(data => data.json())
             .then(data => dataHandler(data))
                 .catch(err => console.log(err));
 
     const dataHandler = (data) => {
         console.log(data);
-        window.location.href = '../../src/views/ticket.html';
+        // window.location.href = '../../src/views/ticket.html';
     };
 })
+
+function frontData(data){
+    const $ul = document.createElement('ul');
+    const $liProduct = document.createElement('li');
+    const $labelQuantity = document.createElement('label');
+    const $deleteButton = document.createElement('button');
+
+    $labelQuantity.className = 'quantity-products';
+    $labelQuantity.id = data.quantity;
+    $labelQuantity.textContent = `Cantidad: ${data.quantity}`;
+
+    $liProduct.className = 'bought-products';
+    $liProduct.id = data.id;
+    $liProduct.textContent = `${data.name}: $${data.price}`;
+
+    $deleteButton.onclick = () => deleteItemCart(data.id);
+    $deleteButton.textContent = 'Eliminar del carrito';
+
+    $liProduct.appendChild($labelQuantity);
+    $liProduct.appendChild($deleteButton);
+    $ul.appendChild($liProduct);
+    $listaProductos.appendChild($ul);
+}
+
+function deleteItemCart(){
+
+}
--- public/purchase-handler.php
+++ public/purchase-handler.php
@@ -8,33 +8,51 @@ require_once __DIR__ . '/../src/bootstrap.php';
 require_once __DIR__ . '/../src/Service/TicketService.php';
 require_once __DIR__ . '/../src/Service/purchaseService.php';
 
-$userEMail = $_POST['useEMail'];
-$productsID = $_POST['userMail'];
-$quantity = $_POST['userMail'];
+$_POST = json_decode(file_get_contents('php://input'), true);
 
-$ticketService = new TicketService($entityManager);
-$purchaseService = new purchaseService($entityManager);
+$action = isset($_POST['action']) ? $_POST['action'] : null;
+$productsID = isset($_POST['productsID']) ? $_POST['productsID'] : [];
 
-$count = 0;
-
-try{
-    $user = $entityManager->getRepository(UserData::class)->findBy(array('email'=>$userEMail));
-    $ticket = $ticketService->createTicket($user[0], null);
-    
-    foreach($productsID as $productID){
-        $product = $entityManager->getRepository(Product::class)->findBy(array('id'=>$productID));
-        $purchase = $purchaseService->addPurchase($product[0], $quantity[$count], $ticket);
-        $count += 1;
+foreach($productsID as $productID){
+    $product = $entityManager->getRepository(Product::class)->find($productID);
+    if ($product) {
+        $products = [
+            'id'    => $product->getId(),
+            'name'  => $product->getName(),
+            'price' => $product->getPrice(),
+        ];
     }
+    echo json_encode($products);
+}
 
-    $response = [
-        'message' => 'success',
-        'Id' => $ticket->getId(),
-        'Date' => $ticket->getDateTime()
-    ];
-
-    return json_encode($response); 
-
-}catch(Throwable $error){
-    return $error->getMessage();
+if($action === 'buy'){
+    $userEMail = $_POST['useEMail'];
+    $quantity = $_POST['quantity'];
+    
+    $ticketService = new TicketService($entityManager);
+    $purchaseService = new purchaseService($entityManager);
+    
+    $count = 0;
+    
+    try{
+        $user = $entityManager->getRepository(UserData::class)->findBy(array('email'=>$userEMail));
+        $ticket = $ticketService->createTicket($user[0], null);
+        
+        foreach($productsID as $productID){
+            $product = $entityManager->getRepository(Product::class)->findBy(array('id'=>$productID));
+            $purchase = $purchaseService->addPurchase($product[0], $quantity[$count], $ticket);
+            $count += 1;
+        }
+    
+        $response = [
+            'message' => 'success',
+            'Id' => $ticket->getId(),
+            'Date' => $ticket->getDateTime()
+        ];
+    
+        return json_encode($response); 
+    
+    }catch(Throwable $error){
+        return $error->getMessage();
+    }
 }
